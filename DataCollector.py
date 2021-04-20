cd08de177fd9baf411c7396fc3d80275c749a864
FA146 <69723973+FardinAhsan146@users.noreply.github.com>
2021-04-19 22:55:24 -0700

    Update DataCollector.py

--- DataCollector.py
+++ DataCollector.py
@@ -9,15 +9,10 @@ def search(sub,word,n_posts):
     print(f"\nConnecting to {sub} \nGathering submissions...")    
 
     for submission in subreddit.hot(limit = n_posts):
-        
         submission.comments.replace_more(limit = None)
-        
         for comment in submission.comments.list():
-            
             comment_count += 1
-            
             if hasattr(comment, "body") and word in comment.body.lower():
- 
                 found_count += 1
                                
     percentage = found_count / comment_count * 100  
@@ -25,7 +20,6 @@ def search(sub,word,n_posts):
     print(f"\nThere was a match {found_count} " \
           f"times in {percentage:.02f}% of the comments " \
               f"scanned in r/{subreddit}. {comment_count} comments scanned.")
-    
     return (found_count,comment_count,percentage)
     
 if __name__ == "__main__":
