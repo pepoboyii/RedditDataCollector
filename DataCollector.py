1afa0bd458cdaaae9805f699c4633d116d512631
FA146 <69723973+FardinAhsan146@users.noreply.github.com>
2021-04-20 21:24:07 -0700

    Reddit comment crawler (#1)

--- DataCollector.py
+++ DataCollector.py
@@ -1,153 +1,50 @@
 import praw
+from matplotlib import pyplot as plt
+   
 
-reddit = praw.Reddit(client_id = "",
- client_secret = "",
- user_agent = "DataCollector")
-
-word = input("Enter word to be searched: ")
-
-def Pics():
-    print("Connecting to subreddit...")
-    subreddit = reddit.subreddit("pics")
-    global pics_x
-    pics_x = 0
-    global pics_y
-    pics_y = 0
-    post = 0
-    print("\n" + "Gathering submissions...")
-    for submission in subreddit.top(limit = 50):
-        post += 1 
-        print("\n" + f"Gathering comments in post #{post}...")        
+def search(sub,word,n_posts):
+    subreddit = reddit.subreddit(sub)
+    
+    comment_count = 0
+    found_count = 0
+    
+    print(f"\nConnecting to {sub} \nGathering submissions...")    
+    
+    for submission in subreddit.hot(limit = n_posts):
         submission.comments.replace_more(limit = None)
         for comment in submission.comments.list():
-            pics_x += 1
-            print("\n" + f"Checking comment #{pics_x}...")
-            body_lower = comment.body.lower()
-            if hasattr(comment, "body"):
-                print("\n" + "Checking for matches...")
-                if word in body_lower:
-                    pics_y += 1
-                    print("\n" + f"Match found. Comment: {comment.body}")
-                else:
-                    print("\n" + "Match not found.")
-    global pics_percentage
-    pics_percentage = pics_y / pics_x * 100            
-
-def Funny():
-    print("Connecting to subreddit...")
-    subreddit = reddit.subreddit("funny")
-    global funny_x
-    funny_x = 0
-    global funny_y
-    funny_y = 0
-    post = 0
-    print("\n" + "Gathering submissions...")
-    for submission in subreddit.top(limit = 50):
-        post += 1 
-        print("\n" + f"Gathering comments in post #{post}...")        
-        submission.comments.replace_more(limit = None)
-        for comment in submission.comments.list():
-            funny_x += 1
-            print("\n" + f"Checking comment #{funny_x}...")
-            body_lower = comment.body.lower()
-            if hasattr(comment, "body"):
-                print("\n" + "Checking for matches...")
-                if word in body_lower:
-                    funny_y += 1
-                    print("\n" + f"Match found. Comment: {comment.body}")
-                else:
-                    print("\n" + "Match not found.")
-    global funny_percentage
-    funny_percentage = funny_y / funny_x * 100
-
-def Politics():
-    print("Connecting to subreddit...")
-    subreddit = reddit.subreddit("politics")
-    global politics_x
-    politics_x = 0
-    global politics_y
-    politics_y = 0
-    post = 0
-    print("\n" + "Gathering submissions...")
-    for submission in subreddit.top(limit = 50):
-        post += 1 
-        print("\n" + f"Gathering comments in post #{post}...")        
-        submission.comments.replace_more(limit = None)
-        for comment in submission.comments.list():
-            politics_x += 1
-            print("\n" + f"Checking comment #{politics_x}...")
-            body_lower = comment.body.lower()
-            if hasattr(comment, "body"):
-                print("\n" + "Checking for matches...")
-                if word in body_lower:
-                    politics_y += 1
-                    print("\n" + f"Match found. Comment: {comment.body}")
-                else:
-                    print("\n" + "Match not found.")
-    global politics_percentage
-    politics_percentage = politics_y / politics_x * 100
-
-def Gaming():
-    print("Connecting to subreddit...")
-    subreddit = reddit.subreddit("gaming")
-    global gaming_x
-    gaming_x = 0
-    global gaming_y
-    gaming_y = 0
-    post = 0
-    print("\n" + "Gathering submissions...")
-    for submission in subreddit.top(limit = 50):
-        post += 1 
-        print("\n" + f"Gathering comments in post #{post}...")        
-        submission.comments.replace_more(limit = None)
-        for comment in submission.comments.list():
-            gaming_x += 1
-            print("\n" + f"Checking comment #{gaming_x}...")
-            body_lower = comment.body.lower()
-            if hasattr(comment, "body"):
-                print("\n" + "Checking for matches...")
-                if word in body_lower:
-                    gaming_y += 1
-                    print("\n" + f"Match found. Comment: {comment.body}")
-                else:
-                    print("\n" + "Match not found.")
-    global gaming_percentage
-    gaming_percentage = gaming_y / gaming_x * 100
-
-def AskReddit():
-    print("Connecting to subreddit...")
-    subreddit = reddit.subreddit("askreddit")
-    global ask_reddit_x
-    ask_reddit_x = 0
-    global ask_reddit_y
-    ask_reddit_y = 0
-    post = 0
-    print("\n" + "Gathering submissions...")
-    for submission in subreddit.top(limit = 50):
-        post += 1 
-        print("\n" + f"Gathering comments in post #{post}...")        
-        submission.comments.replace_more(limit = None)
-        for comment in submission.comments.list():
-            ask_reddit_x += 1
-            print("\n" + f"Checking comment #{ask_reddit_x}...")
-            body_lower = comment.body.lower()
-            if hasattr(comment, "body"):
-                print("\n" + "Checking for matches...")
-                if word in body_lower:
-                    ask_reddit_y += 1
-                    print("\n" + f"Match found. Comment: {comment.body}")
-                else:
-                    print("\n" + "Match not found.")
-    global ask_reddit_percentage
-    ask_reddit_percentage = ask_reddit_y / ask_reddit_x * 100
-Pics()
-Funny()
-Politics()
-Gaming()
-AskReddit()
-print("\n" + f"There was a match {pics_y} times in {pics_percentage}% of the data in r/Pics. {pics_x} comments scanned.")  
-print(f"There was a match {funny_y} times in {funny_percentage}% of the data in r/Funny. {funny_x} comments scanned.") 
-print(f"There was a match {politics_y} times in {politics_percentage}% of the data in r/Politics. {politics_x} comments scanned.")  
-print(f"There was a match {gaming_y} times in {gaming_percentage}% of the data in r/Gaming. {gaming_x} comments scanned.")
-print(f"There was a match {ask_reddit_y} times in {ask_reddit_percentage}% of the data in r/AskReddit. {ask_reddit_x} comments scanned.")
-input("Press enter to close: ")  
+            comment_count += 1
+            if hasattr(comment, "body") and word in comment.body.lower():
+                found_count += 1
+                               
+    percentage = found_count / comment_count * 100  
+    
+    print(f"\nThere was a match {found_count} " \
+          f"times in {percentage:.02f}% of the comments " \
+              f"scanned in r/{subreddit}. {comment_count} comments scanned.")
+    return (found_count,comment_count,percentage)
+    
+if __name__ == "__main__":
+    
+    #The things here are reddit secret information, get these from the reddit website developer section
+    reddit = praw.Reddit(client_id = "",
+                     client_secret = "",
+                     user_agent = "")
+    
+    #populate this list with the subreddits you want to search for
+    sub_list = ['pics','politics','funny']
+    
+    #This is obvious
+    word_to_search = 'why'
+   
+    nof_posts = 1
+      
+    D = {} #Dict for plotting
+    
+    #Plotting
+    for subreddit in sub_list:
+        D[subreddit] = search(subreddit,word_to_search,nof_posts)[2]
+        
+    plt.bar(range(len(D)), list(D.values()), align='center')
+    plt.title(f'% comments \'{word_to_search}\' showed up in')
+    plt.xticks(range(len(D)), list(D.keys()))
